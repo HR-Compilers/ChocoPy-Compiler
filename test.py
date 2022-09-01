a68f793ac3fdb531b0bda5439768c18b61d6f16a
Melon <matteobecatti7@gmail.com>
2022-09-01 13:30:43 +0000

    Edited test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+None pass break continue import 
+    from as class if elif else while def return 
+        del assert global nonlocal 
+            try except raise finally async away yield lambda 
+                or and not is in
++ - * // %
+
+<><=>===!== ()[] , : . -> 
+
+True False 69 45 1234 420 
+
+"Good morning, how are \nyou doing today?"
+
+int a = 5
