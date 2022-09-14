39d7c7a3e562374e89fe5139b23f0a97f50ab901
BasLaa <baslaarakker@gmail.com>
2022-09-14 15:13:10 +0000

    more grammar done

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 # with target: parse as expr, check if you see = after
 # check to make sure that the expr matches target
 
+from lib2to3.pgen2.token import tok_name
 from lexer import Lexer, Tokentype, SyntaxErrorException
 import ast
 
@@ -46,7 +47,7 @@ class Parser:
     # program ::= [[var def | func def | class def]]* stmt*
     def program(self):
         # we need one more lookahead for var_def as stmt can start with ID as well
-        # we need to peek if the next token is a colon: elif self.peek() == colon
+        # we need to peek if the next token is a colon
         while self.token.type in [Tokentype.KwDef, Tokentype.KwClass, Tokentype.Identifier]:
             if self.token.type == Tokentype.KwClass:
                 self.class_def()
@@ -58,7 +59,7 @@ class Parser:
                 else:
                     break
         
-        while self.token.type is not Tokentype.EOI:
+        while self.token.type != Tokentype.EOI:
             self.stmt()
 
     # class_def ::= class ID ( ID ) : NEWLINE INDENT class_body DEDENT
@@ -74,8 +75,7 @@ class Parser:
         self.class_body()
         self.match(Tokentype.Dedent)
 
-    # class_body ::= pass NEWLINE
-    #               | [[var_def | func_def]]+
+    # class_body ::= pass NEWLINE | [[var_def | func_def]]+
     def class_body(self):
         if self.token.type == Tokentype.KwPass:
             self.match(Tokentype.KwPass)
@@ -94,7 +94,7 @@ class Parser:
                 else:
                     self.var_def()
 
-    # def ID ( [[typed var [[, typed var]]* ]]? ) [[-> type]]? : NEWLINE INDENT func_body DEDENT
+    # func_def ::= def ID ( [[typed var [[, typed var]]* ]]? ) [[-> type]]? : NEWLINE INDENT func_body DEDENT
     def func_def(self):
         self.match(Tokentype.kwDef)
         self.match(Tokentype.Identifier)
@@ -117,21 +117,102 @@ class Parser:
         self.func_body()
         self.match(Tokentype.Dedent)
         
-    # [[global_decl | nonlocal_decl | var def | func def 􏰲 stmt]]* stmt+
+    # func_body ::= [[global_decl | nonlocal_decl | var def | func def]]* stmt+
     def func_body(self):
-        ...
+        while self.token.type in [Tokentype.KwGlobal, Tokentype.KwNonLocal, Tokentype.KwDef, Tokentype.Identifier]:
+            if self.token.type == Tokentype.KwGlobal:
+                self.global_decl()
+            elif self.token.type == Tokentype.KwNonLocal:
+                self.nonlocal_decl()
+            elif self.token.type == Tokentype.KwDef:
+                self.func_def()
+            # Identifier
+            else:
+                if self.peek() == Tokentype.Colon:
+                    self.var_def()
+                else:
+                    # move on to stmt
+                    break
+        
+        # need one or more statements
+        self.stmt()
+        while self.token.type != Tokentype.Dedent:
+            self.stmt()
     
+    # typed_var ::= ID : type
     def typed_var(self):
-        ...
+        self.match(Tokentype.Identifier)
+        self.match(Tokentype.Colon)
+        self._type()
     
+    # type ::= ID | IDSTRING | [ type ]
     def _type(self):
-        ...
+        if self.match_if(Tokentype.BracketL):
+            self._type()
+            self.match(Tokentype.BracketR)
+        else:
+            self.match(Tokentype.Identifier)
 
+    # global_decl ::= global ID NEWLINE
+    def global_decl(self):
+        self.match(Tokentype.KwGlobal)
+        self.match(Tokentype.Identifier)
+        self.match(Tokentype.Newline)
+    
+    # nonlocal_decl ::= nonlocal ID NEWLINE
+    def nonlocal_decl(self):
+        self.match(Tokentype.KwNonLocal)
+        self.match(Tokentype.Identifier)
+        self.match(Tokentype.Newline)
+
+    # var_def ::= typed var = literal NEWLINE
     def var_def(self):
         self.typed_var()
         self.match(Tokentype.OpEq)
         self.literal()
         self.match(Tokentype.Newline)
 
+    # stmt ::= simple_stmt NEWLINE
+    # | if expr : block [[elif expr : block]]* [[else : block]]?
+    # | while expr : block
+    # | for ID in expr : block
     def stmt(self):
+        if self.match_if(Tokentype.KwIf):
+            self.expr()
+            self.match(Tokentype.Colon)
+            self.block()
+            while self.match_if(Tokentype.KwElif):
+                self.expr()
+                self.match(Tokentype.Colon)
+                self.block()
+            if self.match_if(Tokentype.KwElse):
+                self.match(Tokentype.Colon)
+                self.block()
+
+        elif self.match_if(Tokentype.KwWhile):
+            self.expr()
+            self.match(Tokentype.Colon)
+            self.block()
+
+        elif self.match_if(Tokentype.KwFor):
+            self.match(Tokentype.Identifier)
+            self.match(Tokentype.OpIn)
+            self.expr()
+            self.match(Tokentype.Colon)
+            self.block()
+
+        else:
+            self.simple_stmt()
+            self.match(Tokentype.Newline)
+
+    def simple_stmt(self):
+        ...
+    
+    def block(self):
+        ...
+    
+    def literal(self):
+        ...
+    
+    def expr(self):
         ...
