4a3121c287da265a4f47b88597b5b90bab9ebf75
BasLaa <baslaarakker@gmail.com>
2022-09-18 17:20:48 +0000

    Added ordering to the dir files

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import print_visitor
 mypath = "tests/"
 files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 filtered_files = ["tests/" + element for element in files if element.endswith('.py') and not element.startswith('bad')]
+filtered_files.sort()
 
 for filename in filtered_files:
     with open(filename) as f:
