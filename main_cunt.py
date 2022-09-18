20ba32eaf60a7d75d57feee509f35aac59977608
Matteo Becatti <79763286+meloncruuush@users.noreply.github.com>
2022-09-18 19:11:50 +0000

    Added second main file

---
+++ main_cunt.py
@@ -0,0 +1,24 @@
+from fileinput import filename
+from os import listdir
+from os.path import isfile, join
+
+import parser as parser
+import lexer as lexer
+import print_visitor
+
+# We took all the test files from the github repository of ChocoPy
+# Here we iterate over them and parse each of them as test
+mypath = "tests/"
+files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+filtered_files = ["tests/" + element for element in files if element.endswith('.py') and not element.startswith('bad')]
+filtered_files.sort()
+
+#for filename in filtered_files:
+filename = "tests/coverage.py"
+with open(filename) as f:
+    print('\n' + filename)
+    p = parser.Parser(f)
+    node = p.parse()
+    pv = print_visitor.PrintVisitor()
+    pv.do_visit(node)
+    print("fully parsed file")
